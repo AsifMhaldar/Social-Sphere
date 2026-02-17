e21bd600b074f8b516df63eb801b0647618e895d
Asif <asif.mhaldar03@gmail.com>
2026-02-17 13:22:29 +0530

    resolve

--- Backend/src/socket/socket.js
+++ Backend/src/socket/socket.js
@@ -6,7 +6,8 @@ let onlineUsers = [];
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.NETLIFY_FRONTEND,
+      // origin: "http://localhost:5173",
       credentials: true,
     },
   });
