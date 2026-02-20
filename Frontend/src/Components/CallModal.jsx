411aa4a2976b4f1d6e20e303e1b4efe75101e2c8
Asif <asif.mhaldar03@gmail.com>
2026-02-20 16:34:34 +0530

    Ok

--- Frontend/src/Components/CallModal.jsx
+++ Frontend/src/Components/CallModal.jsx
@@ -22,7 +22,16 @@ export default function CallModal({
   const [isRinging, setIsRinging] = useState(false);
 
   const servers = {
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+    iceServers: [
+      {
+        urls: "stun:stun.l.google.com:19302",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:80",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+    ],
   };
 
   // ================================
